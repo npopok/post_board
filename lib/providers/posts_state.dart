c9f39208f4ae7da417e02789f5a2c7daca03389f
Nikolay Popok <nick.popok@gmail.com>
2024-06-04 18:12:04 +0700

    Remove unnecassary caching

--- lib/providers/posts_state.dart
+++ lib/providers/posts_state.dart
@@ -1,10 +1,8 @@
 import 'package:post_board/providers/providers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import 'package:post_board/common/common.dart';
 import 'package:post_board/models/models.dart';
 import 'package:post_board/repositories/repositories.dart';
-import 'package:post_board/helpers/helpers.dart';
 
 part 'posts_state.g.dart';
 
@@ -14,8 +12,6 @@ class PostsState extends _$PostsState {
   Future<Posts> build() => loadData();
 
   Future<Posts> loadData() async {
-    ref.cacheFor(RepositorySettings.postsCacheDuration);
-
     final filters = ref.watch(filtersStateProvider);
     return remoteRepository.loadPosts(filters, null);
   }
